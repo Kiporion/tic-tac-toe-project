2d6fa800ace9508bc0d82194f3a4bed9a86f78d1
Meho Mustafic <90556836+Kiporion@users.noreply.github.com>
2023-03-01 14:30:47 +0100

    Finished simplified game version

--- script.js
+++ script.js
@@ -1,44 +1,76 @@
-// Player Object
-const player = (name, mark) => {
-  return { name, mark };
-};
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#statusText");
+const restartBtn = document.querySelector("#restart");
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
 
-// Main Game Function
-const GameBoard = (() => {
-  const board = ["x", "", "", "", "", "", "y", "", ""];
-  // Login / Player Select
-  function login() {
-    const playerOne = player(document.querySelector("#player-one").value, "X");
-    const playerTwo = player(document.querySelector("#player-two").value, "X");
+initializeGame();
+
+function initializeGame() {
+  cells.forEach((cell) => cell.addEventListener("click", cellClicked));
+  restartBtn.addEventListener("click", restartGame);
+  statusText.textContent = `${currentPlayer}'s turn`;
+  running = true;
+}
+function cellClicked() {
+  const cellIndex = this.getAttribute("cellIndex");
+  if (options[cellIndex] != "" || !running) {
+    return;
   }
 
-  function placeMark(index) {
-    console.log(`place ${index}`);
-    board[index].value = "X";
-  }
-  // Board Generator / Render
-  function render() {
-    let gBoard = document.querySelector(".game-board");
-    gBoard.innerHTML = "";
-    for (let i = 0; i < board.length; i++) {
-      //   let square = document.createElement("button");
-      //   square.setAttribute("class", "square");
-      //   square.innerHTML = board[i];
-      //   square.addEventListener("onClick", placeMark(i));
-      let square = document.createElement("div");
-      square.setAttribute("class", "square");
-      square.innerHTML = `<button class="sq-btn" onClick="placeMark(${i})"></button>`;
-      placeMark(i);
-      gBoard.appendChild(square);
+  updateCell(this, cellIndex);
+  checkWinner();
+}
+function updateCell(cell, index) {
+  options[index] = currentPlayer;
+  cell.textContent = currentPlayer;
+}
+function changePlayer() {
+  currentPlayer = currentPlayer == "X" ? "O" : "X";
+  statusText.textContent = `${currentPlayer}'s turn`;
+}
+function checkWinner() {
+  let roundWon = false;
+
+  for (let i = 0; i < winConditions.length; i++) {
+    const condition = winConditions[i];
+    const cellA = options[condition[0]];
+    const cellB = options[condition[1]];
+    const cellC = options[condition[2]];
+
+    if (cellA == "" || cellB == "" || cellC == "") {
+      continue;
+    }
+    if (cellA == cellB && cellB == cellC) {
+      roundWon = true;
+      break;
     }
   }
-  // Form Submit
-  document
-    .querySelector("#sign-in")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
-      login();
-      this.style = "display:none;";
-    });
-  render();
-})();
+  if (roundWon) {
+    statusText.textContent = `${currentPlayer} wins!`;
+    running = false;
+  } else if (!options.includes("")) {
+    statusText.textContent = `Draw!`;
+    running = false;
+  } else {
+    changePlayer();
+  }
+}
+function restartGame() {
+  currentPlayer = "X";
+  options = ["", "", "", "", "", "", "", "", ""];
+  statusText.textContent = `${currentPlayer}' turn`;
+  cells.forEach((cell) => (cell.textContent = ""));
+  running = true;
+}
