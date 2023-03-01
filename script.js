d500d66267604c087530a62f3eb2492a6bbf29c0
Meho Mustafic <90556836+Kiporion@users.noreply.github.com>
2023-03-01 13:08:44 +0100

    added GameBoard function and Player objects

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("hello world");
+// Player Object
+const player = (name, mark) => {
+  return { name, mark };
+};
+
+// Main Game Function
+const GameBoard = (() => {
+  const board = ["x", " ", "o", "o", "x", " ", "o", "x", "o"];
+  // Login / Player Select
+  function login() {
+    const playerOne = player(document.querySelector("#player-one").value, "X");
+    const playerTwo = player(document.querySelector("#player-two").value, "X");
+  }
+
+  // Board Generator / Render
+  function render() {
+    let gBoard = document.querySelector(".game-board");
+    gBoard.innerHTML = "";
+    for (let i = 0; i < board.length; i++) {
+      let square = document.createElement("div");
+      square.setAttribute("class", "square");
+      square.innerText = `${board[i]}`;
+      gBoard.appendChild(square);
+    }
+  }
+  document
+    .querySelector("#sign-in")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      login();
+    });
+  render();
+})();
