5c1a826c99cc6b59377d768df0b02d26fefa9004
Meho Mustafic <90556836+Kiporion@users.noreply.github.com>
2023-03-01 13:38:50 +0100

    implementing onClick and game logic

--- script.js
+++ script.js
@@ -5,29 +5,40 @@ const player = (name, mark) => {
 
 // Main Game Function
 const GameBoard = (() => {
-  const board = ["x", " ", "o", "o", "x", " ", "o", "x", "o"];
+  const board = ["x", "", "", "", "", "", "y", "", ""];
   // Login / Player Select
   function login() {
     const playerOne = player(document.querySelector("#player-one").value, "X");
     const playerTwo = player(document.querySelector("#player-two").value, "X");
   }
 
+  function placeMark(index) {
+    console.log(`place ${index}`);
+    board[index].value = "X";
+  }
   // Board Generator / Render
   function render() {
     let gBoard = document.querySelector(".game-board");
     gBoard.innerHTML = "";
     for (let i = 0; i < board.length; i++) {
+      //   let square = document.createElement("button");
+      //   square.setAttribute("class", "square");
+      //   square.innerHTML = board[i];
+      //   square.addEventListener("onClick", placeMark(i));
       let square = document.createElement("div");
       square.setAttribute("class", "square");
-      square.innerText = `${board[i]}`;
+      square.innerHTML = `<button class="sq-btn" onClick="placeMark(${i})"></button>`;
+      placeMark(i);
       gBoard.appendChild(square);
     }
   }
+  // Form Submit
   document
     .querySelector("#sign-in")
     .addEventListener("submit", function (event) {
       event.preventDefault();
       login();
+      this.style = "display:none;";
     });
   render();
 })();
